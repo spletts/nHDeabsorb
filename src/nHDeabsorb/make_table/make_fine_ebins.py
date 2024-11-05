0d5c4f149ddeab871fd0d82b5a03aa920d3670a3
Megan Splettstoesser <mspletts@ucsc.edu>
2024-11-05 09:13:43 -0800

    bug: save df with header=None to remove the first line of ebin_edges.txt (0, index)

--- src/nHDeabsorb/make_table/make_fine_ebins.py
+++ src/nHDeabsorb/make_table/make_fine_ebins.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 
 # Exponent for bin size (also for rounding)
-expo = 3
+expo = 4
 bin_width = 2*10**-expo  # keV
 
 # XSpec will calculate the middle i.e. the first bin is 0.3
-emin, emax = 0.3-bin_width, 10+bin_width  # keV
+emin, emax = 0.3, 10  # keV
 
 # Round because:
 # (Pdb) 9.7/0.01
@@ -17,4 +17,4 @@ emin, emax = 0.3-bin_width, 10+bin_width  # keV
 num_bins = round((emax - emin)/bin_width)
 ebins = np.round(np.linspace(emin, emax, num_bins), expo)
 df = pd.DataFrame(data=ebins)
-df.to_csv('ebin_edges.txt', index=False)
+df.to_csv('ebin_edges.txt', index=False, header=None)
