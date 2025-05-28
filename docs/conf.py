2fc1472f603227dcc66bb95577ea4035c1d3ba69
Megan Splettstoesser <mspletts@ucsc.edu>
2025-05-28 09:05:52 -0700

    update release number

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 project = 'nHDeabsorb'
 copyright = '2025, Megan Splettstoesser'
 author = 'Megan Splettstoesser'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
